3a19ada35fb0bb886a209b8cdcb0712ee38e08b6
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-01-26 17:49:24 -0500

    Update Utils.php

--- src/vale/utils/Utils.php
+++ src/vale/utils/Utils.php
@@ -23,11 +23,11 @@ class Utils extends BaseCrate
 {
 
 	/** @var SharedInvMenu $menu */
-	public static $menu;
-	public static $row1 = [9,10,11,12,13,14,15,16,17];
-	public static $row2 = [18,19,20,21,22,23,24,25,26];
-	public static $row3 = [27,28,29,30,31,32,33,34,35];
-	public static $row4 = [36,37,38,39,40,41,42,43,45];
+	public static SharedInvMenu $menu;
+	public static array $row1 = [9,10,11,12,13,14,15,16,17];
+	public static array $row2 = [18,19,20,21,22,23,24,25,26];
+	public static array $row3 = [27,28,29,30,31,32,33,34,35];
+	public static array $row4 = [36,37,38,39,40,41,42,43,45];
 
 	public static function sendMenu(Player $player, string $type)
 	{
